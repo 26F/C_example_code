de47660d87124f6c8f037dfb748b5f5b11de5c71
623 <32969567+26F@users.noreply.github.com>
2019-01-04 19:08:03 +1300

    Create 009 strtok.c

---
+++ 009 strtok.c
@@ -0,0 +1,24 @@
+// separate words from single line using fgets and strtok
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   FILE * fp = fopen("doc.txt", "r"); // open file that already exists (in read mode)
+   if (fp == NULL) {
+      perror("Could not open file");
+      return -1;
+   }
+   char buffer[1024];
+   if (fgets(buffer, 1024, fp) == NULL) {
+      printf("Failed to read string from file");
+      return -1;
+   }
+   char delimiters[] = ";,";
+   char * word = strtok(buffer, delimiters);
+   while (word) {
+      printf("%s\n", word);
+      word = strtok(NULL, delimiters);
+   }
+   fclose(fp);
+   return 0;
+}
