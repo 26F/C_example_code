7418a7be6edc4b0e3e63eed07fab8e0d1616f21b
623 <32969567+26F@users.noreply.github.com>
2019-01-19 13:57:08 +1300

    Create read and write modes.c

---
+++ read and write modes.c
@@ -0,0 +1,30 @@
+
+/*
+
+stdin  in
+stdout out
+stderr error_logging
+
+A buffer is a temporary storage area that uses streams to move and store data.
+
+fopen()
+fclose()
+
+*/
+
+int main() {
+  // FILE *fp;
+  // fp = fopen("names.txt","r");
+  // loads file data into a buffer.
+  // fopen() creates a stream and the buffer is associated with that stream.
+  // fp is a pointer to the base address of the stream.
+
+  // r read mode. Open the file if it exist, else return a NULL pointer.
+  // w write mode. Overwrite the file. 
+  // a append mode. returns a NULL pointer if the file does not exist.
+
+  // add a + to create a new file, for example w+ .
+
+  // using wb is fine for writing new files.
+
+}
